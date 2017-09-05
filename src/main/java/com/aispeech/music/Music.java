1c9b220a55f3bcc68bfd381af6ab90e342153765
chunhua.zeng <chunhua.zeng@aispeech.com>
2017-09-05 12:30:28 +0800

    first commit

---
+++ src/main/java/com/aispeech/music/Music.java
@@ -0,0 +1,15 @@
+package com.aispeech.music;
+
+
+import android.util.Log;
+
+/**
+ * @author Spring  09.05 2017
+ */
+
+public class Music {
+
+    public static void init() {
+        Log.e("Music", "i am music");
+    }
+}
